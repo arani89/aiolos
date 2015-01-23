205c346a72574154756adaadcf86d11430a7c570
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-01-23 13:44:30 +0100

    use BundleTracker instead of BundleListener

--- be.iminds.aiolos.configurer/src/be/iminds/aiolos/configurer/Activator.java
+++ be.iminds.aiolos.configurer/src/be/iminds/aiolos/configurer/Activator.java
@@ -1,20 +1,58 @@
 package be.iminds.aiolos.configurer;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.util.tracker.BundleTracker;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 public class Activator implements BundleActivator {
 
+	private ServiceTracker<ConfigurationAdmin, ConfigurationAdmin> cmTracker;
+	
+	private BundleTracker<Bundle> bundleTracker;
+	
 	@Override
-	public void start(BundleContext context) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void start(final BundleContext context) throws Exception {
+		cmTracker = new ServiceTracker<ConfigurationAdmin, ConfigurationAdmin>(
+				context, ConfigurationAdmin.class, 
+				new ServiceTrackerCustomizer<ConfigurationAdmin, ConfigurationAdmin>() {
+
+					@Override
+					public ConfigurationAdmin addingService(
+							ServiceReference<ConfigurationAdmin> reference) {
+						ConfigurationAdmin cm = (ConfigurationAdmin) context.getService(reference);
+						if(cm!=null){
+							bundleTracker = new BundleTracker<Bundle>(context, Bundle.ACTIVE | Bundle.STARTING | Bundle.STOPPING, new Configurer(cm));
+							bundleTracker.open();
+						}
+						return cm;
+					}
+
+					@Override
+					public void modifiedService(
+							ServiceReference<ConfigurationAdmin> reference,
+							ConfigurationAdmin service) {}
+
+					@Override
+					public void removedService(
+							ServiceReference<ConfigurationAdmin> reference,
+							ConfigurationAdmin service) {
+						bundleTracker.close();
+					}
+		});
+		cmTracker.open();
 	}
 
 	@Override
-	public void stop(BundleContext context) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void stop(final BundleContext context) throws Exception {
+		if(bundleTracker!=null){
+			bundleTracker.close();
+		}
+		cmTracker.close();
 	}
 
 }
--- be.iminds.aiolos.configurer/src/be/iminds/aiolos/configurer/Configurer.java
+++ be.iminds.aiolos.configurer/src/be/iminds/aiolos/configurer/Configurer.java
@@ -1,5 +1,32 @@
 package be.iminds.aiolos.configurer;
 
-public class Configurer {
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleEvent;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.util.tracker.BundleTrackerCustomizer;
+
+public class Configurer implements  BundleTrackerCustomizer<Bundle> {
+
+	private final ConfigurationAdmin cm;
+	
+	public Configurer(ConfigurationAdmin cm){
+		this.cm = cm;
+	}
+
+	@Override
+	public Bundle addingBundle(Bundle bundle, BundleEvent event) {
+		System.out.println("ADD BUNDLE "+bundle.getSymbolicName());
+		return bundle;
+	}
+
+	@Override
+	public void modifiedBundle(Bundle bundle, BundleEvent event, Bundle object) {}
+
+	@Override
+	public void removedBundle(Bundle bundle, BundleEvent event, Bundle object) {
+		System.out.println("REMOVE BUNDLE "+bundle.getSymbolicName());
+	}
+	
+
 
 }
